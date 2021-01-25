3b3f95c1dfe0e95776e5bfb4782a197cbcd5fe9f
managemymilk <madhurmungra2000@gmail.com>
2021-01-25 22:36:50 +0530

    first commit

---
+++ Demo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text,View} from 'react-native'
+ const Demo=(props)=> {
+     return(
+         <View>
+            <Text>
+                who are you {props.name} ? abe oyy
+            </Text>
+         </View>
+     )
+ }
+ export default Demo
